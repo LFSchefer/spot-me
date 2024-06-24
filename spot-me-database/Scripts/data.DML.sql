2d6837aa22cc1c9e522bdb782b4cb979368b99e7
louis frederic schefer <lf.schefer@gmail.com>
2024-06-24 15:37:22 +0200

    secound commit #1

---
+++ spot-me-database/Scripts/data.DML.sql
@@ -0,0 +1,7 @@
+DELETE FROM t_spots;
+
+INSERT INTO t_spots (spot_name,spot_lat,spot_lng) VALUES 
+('Hellfest corners', 48.86049270629883, 2.3502705097198486),
+('Le bouillon belge',48.84734344482422, 2.395885944366455);
+
+SELECT * FROM t_spots ts ;
