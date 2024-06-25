6a5650e292be1d3897e3a44cc4a97f0d2495dfac
louis frederic schefer <lf.schefer@gmail.com>
2024-06-25 10:59:08 +0200

    initial commit with buisness java project

---
+++ spot-me-buisness/src/main/java/co/simplon/spot_me_buisness/SpotMeBuisnessApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.spot_me_buisness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotMeBuisnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotMeBuisnessApplication.class, args);
+	}
+
+}
---
+++ spot-me-buisness/src/test/java/co/simplon/spot_me_buisness/SpotMeBuisnessApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.spot_me_buisness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotMeBuisnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
